c3ef8320c1f2092ad4bf31ca5c26ebbae081d914
Angela Lee <angelalee@Angelas-MacBook-Air.local>
2021-07-16 16:04:02 +0900

    20210716

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <form v-on:submit.prevent="infoReceive">
+<!-- 아이디 구역 1-->
+    <div>
+    <label for="userid">id :</label>
+    <input id="userid" type="text" v-model='userid'>
+    </div>
+<!-- 패스워드 구역 1-->
+    <div>
+    <label for="userpw">pw :</label>
+    <input id="userpw" type="password" v-model='password'>
+    </div>
+
+    <button type="submit">login</button>
+
+  </form>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// npm install --save axios 
+import axios from 'axios';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data:function(){
+    return{
+     // 여기랑 v-model이랑 연결되는 곳
+     userid:"",
+     password:""
+    }
+  },
+  methods:{
+    //function 만들기
+    //1. data 값을 콘솔창에 찍기
+    /*infoReceive:function(){
+      console.log(this.userid,this.password);
+    }*/
+    //2. data 값을 서버로 넘기기
+   infoReceive:function()
+   {
+     var url='https://jsonplaceholder.typicode.com/users';
+     var data={
+       userid:this.userid,
+       password:this.password
+     }
+
+     //Post 새로운 객체 생성용 
+     axios.post(url,data) // axios.post("url 주소",{data 객체})
+      .then(function(response){
+        console.log(response);
+      })
+      .catch(function(error){
+        console.log(error);
+      });
+   }
+    
   }
+  
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
